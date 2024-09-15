7e3373cf838085f6a1c924119aa0d427329910f1
Braden Porter <bradenmporter@gmail.com>
2024-09-14 19:34:22 -0600

    Wrote ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +11,13 @@ package chess;
  */
 public class ChessBoard {
 
+    int maxRow = 8;
+    int maxCol = 8;
+
+    ChessPiece[][] board = new ChessPiece[maxCol][maxRow];
+
     public ChessBoard() {
-        
+
     }
 
     /**
@@ -19,7 +27,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece newPiece = new ChessPiece(piece.getTeamColor(), piece.getPieceType());
+        board[position.getColumn()][position.getRow()] = newPiece;
     }
 
     /**
@@ -30,7 +39,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getColumn()][position.getRow()];
     }
 
     /**
@@ -38,6 +47,76 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        //white pawns
+        for(int i = 0; i < 8; i++){
+            int x = 1;
+            ChessPosition newPos = new ChessPosition(x, i);
+            addPiece(newPos, new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+
+        for(int i = 0; i < 8; i++){
+            int x = 6;
+            ChessPosition newPos = new ChessPosition(x, i);
+            addPiece(newPos, new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+        //white rooks
+        addPiece(new ChessPosition(0,0), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(0,7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        //black rooks
+        addPiece(new ChessPosition(7,0), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(7,7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+
+        //white knights
+        addPiece(new ChessPosition(0,1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(0,6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+
+        //black knights
+        addPiece(new ChessPosition(7,1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(7,6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+
+        //white bishops
+        addPiece(new ChessPosition(0,2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(0,5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+
+        //black bishops
+        addPiece(new ChessPosition(7,2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(7,5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+
+        //White queen
+        addPiece(new ChessPosition(0,3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+
+        //Black queen
+        addPiece(new ChessPosition(7,3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+
+        //White king
+        addPiece(new ChessPosition(0,4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+
+        //Black king
+        addPiece(new ChessPosition(7,4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return maxRow == that.maxRow && maxCol == that.maxCol && Arrays.equals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(maxRow, maxCol);
+        result = 31 * result + Arrays.hashCode(board);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "maxRow=" + maxRow +
+                ", maxCol=" + maxCol +
+                ", board=" + Arrays.toString(board) +
+                '}';
     }
 }
