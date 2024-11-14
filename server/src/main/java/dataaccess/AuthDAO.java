f839b6da24ea1198c3bf64f2caadd8b41437320d
Braden Porter <bradenmporter@gmail.com>
2024-11-13 19:58:45 -0700

    Did MemoryAuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,6 +3,9 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
+    void createAuth(AuthData u) throws DataAccessException;
+    AuthData getAuth(String Auth) throws DataAccessException;
+    void deleteAuth(AuthData u) throws DataAccessException;
 
     //createAuth
     //getAuth
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,5 +1,38 @@
 package dataaccess;
 
 import model.AuthData;
+import model.GameData;
 
-public class MemoryAuthDAO implements AuthDAO{}
+import javax.xml.crypto.Data;
+import java.util.HashSet;
+
+public class MemoryAuthDAO implements AuthDAO{
+
+    HashSet<AuthData> tempDataBase = new HashSet<>();
+
+    @Override
+    public void createAuth(AuthData u) throws DataAccessException{
+        tempDataBase.add(u);
+    }
+
+    @Override
+    public AuthData getAuth(String auth) throws DataAccessException {
+        for(AuthData data : tempDataBase){
+            if(data.authToken().equals(auth)){
+                return data;
+            }
+        }
+        throw new DataAccessException("Account does not exist");
+    }
+
+    @Override
+    public void deleteAuth(AuthData u) throws DataAccessException {
+        for(AuthData data : tempDataBase){
+            if(data.equals(u)){
+                tempDataBase.remove(u);
+                return;
+            }
+        }
+        throw new DataAccessException("Account does not exist");
+    }
+}
